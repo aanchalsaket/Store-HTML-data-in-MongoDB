a301619a715c7c7c1ba4afc1736ba78d32ce9113
aanchalsaket <aanchalsaketggits@gmail.com>
2023-12-27 20:58:24 +0530

    database connected

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+const fs = require("node:fs")
 const express = require("express")
 const mongoose = require("mongoose")
 
 const app = express()
 const port = 3000
 
+mongoose.connect("mongodb://localhost:27017/UserDataDB").then(()=>{
+    console.log("Database Connected")
+}).catch((e)=>{
+    console.log(e)
+    console.log("Database Can't Be Connected")
+})
+
 app.get("/", (req, res)=>{
-    res.send("Bye world!!!")
+    res.send("Bye world!!")
 })
 
 app.listen(port, ()=>{
